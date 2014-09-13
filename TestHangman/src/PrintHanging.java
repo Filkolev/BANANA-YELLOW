4fc08b8e8cecdc48e2ec22af69d4802531112753
AlexanderMarinov894 <alexander.mrnv@gmail.com>
2014-09-13 15:11:01 +0300

    -Added new class(PrintHanging.class) -Changed game rules

---
+++ TestHangman/src/PrintHanging.java
@@ -0,0 +1,67 @@
+public class PrintHanging {
+	public static void main(String[] args) {
+		
+	}
+	
+	public static void PrintMan(int livesLeft){
+		if (livesLeft == 4) {
+			System.out.println("_____");
+			System.out.println("|   |");
+			System.out.println("|   O");
+			System.out.println("|    ");
+			System.out.println("|    ");
+			System.out.println("|    ");
+			System.out.println();
+		}
+		
+		if (livesLeft == 3) {
+			System.out.println("_____");
+			System.out.println("|   |");
+			System.out.println("|   O");
+			System.out.println("|   | ");
+			System.out.println("|    ");
+			System.out.println("|    ");
+			System.out.println();
+		}
+		
+		if (livesLeft == 2) {
+			System.out.println("_____");
+			System.out.println("|   |");
+			System.out.println("|   O");
+			System.out.println("|  /| ");
+			System.out.println("|    ");
+			System.out.println("|    ");
+			System.out.println();
+		}
+		
+		if (livesLeft == 1) {
+			System.out.println("_____");
+			System.out.println("|   |");
+			System.out.println("|   O");
+			System.out.println("|  /|\\ ");
+			System.out.println("|    ");
+			System.out.println("|    ");
+			System.out.println();
+		}
+		
+		if (livesLeft == 0) {
+		   System.out.println("_____");
+		   System.out.println("|   |");
+		   System.out.println("|   o");
+		   System.out.println("|  /|\\ ");
+		   System.out.println("|    \\");
+		   System.out.println("|    ");
+		   System.out.println();
+		}
+		
+		if (livesLeft < 0) {
+		   System.out.println("_____");
+		   System.out.println("|   |");
+		   System.out.println("|   o");
+		   System.out.println("|  /|\\ ");
+		   System.out.println("|  / \\");
+		   System.out.println("|    ");
+		   System.out.println();
+		}
+	}
+}
