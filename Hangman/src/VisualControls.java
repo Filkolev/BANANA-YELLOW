0cbe9c4176651d9692ee095c9b2a9070f247dc03
Filip Kolev <fil.kolev@gmail.com>
2014-09-16 16:46:08 +0300

    Ordered methods in class
    Put all methods in logical order.

--- Hangman/src/VisualControls.java
+++ Hangman/src/VisualControls.java
@@ -2,6 +2,27 @@ import java.io.IOException;
 
 public class VisualControls {
 	
+	public static void initializeNewGame() {
+		VisualControls.clearConsole(); 
+		VisualControls.printRules();
+		VisualControls.printCategorySelection();		
+	}
+	
+	public static void printRules(){
+		System.out.println("RULES:");
+		System.out.println("1. Enter letters from the English alphabet in order to guess the word!");
+		System.out.println("2. If you enter more than one letter, only the first one will matter.");
+		System.out.println("3. Entering symbols or digits may get you killed!");
+		System.out.println("4. You can make 6 wrong guesses, on the 6th you DIE!");		
+		System.out.println();
+	}
+	
+	public static void printCategorySelection(){
+		System.out.print("\nReady? ");		
+		System.out.println("Choose a category (enter a number or the category name):\n1. Demo\n2. Animals\n"
+				+ "3. Capitals\n4. Fruits and Vegitables\n");
+	}
+	
 	public static void refreshWindow(Integer livesLeft, char[] guessed, boolean repeated, char guessLetter, boolean letterGuessed, boolean gameStarted) {
 		clearConsole();
 		Drawings.PrintMan(livesLeft);
@@ -21,22 +42,7 @@ public class VisualControls {
 		for(int clear = 0; clear < 150; clear++){
 			System.out.println("\b") ;
 		}
-	}
-	
-	public static void printCategorySelection(){
-		System.out.print("\nReady? ");		
-		System.out.println("Choose a category (enter a number or the category name):\n1. Demo\n2. Animals\n"
-				+ "3. Capitals\n4. Fruits and Vegitables\n");
-	}
-	
-	public static void printRules(){
-		System.out.println("RULES:");
-		System.out.println("1. Enter letters from the English alphabet in order to guess the word!");
-		System.out.println("2. If you enter more than one letter, only the first one will matter.");
-		System.out.println("3. Entering symbols or digits may get you killed!");
-		System.out.println("4. You can make 6 wrong guesses, on the 6th you DIE!");		
-		System.out.println();
-	}
+	}	
 	
 	private static void printGuessWord(char[] guessed){
 		System.out.println();
@@ -47,11 +53,35 @@ public class VisualControls {
 		System.out.println();
 	}
 	
+	private static void guessOutcome(boolean letterGuessed, boolean repeated) {		
+		if (repeated) {
+			System.out.println("You already tried this one...");
+		} else if (letterGuessed) {
+			System.out.println("Good guess! Keep going!");
+		} else {
+			System.out.println("Sorry! This letter isn't in the word!");
+		}
+		
+		System.out.println();		
+	}
+	
 	public static void printDeath(String currentWord) {
 		clearConsole();
 		Drawings.PrintMan(0);
 		System.out.println("Sometimes win, sometimes lyun!");
 		System.out.println("The word was: " + currentWord);	
+		newGamePrompt();
+	}
+	
+	public static void printVictory() {
+		clearConsole();
+		try {
+			Drawings.printSmiley();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("YOU GUESSED THE WORD !!!");	
 		newGamePrompt();
 	}
 	
@@ -69,39 +99,9 @@ public class VisualControls {
 		System.out.println("\nIf you ran out of words you can purchase more words for the DISCOUNTED price of JUST $0.99! Call your local representative now!");
 		System.out.println();		
 		System.out.println("Press Enter to exit...");
-	}
-	
-	public static void printVictory() {
-		clearConsole();
-		try {
-			Drawings.printSmiley();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.out.println("YOU GUESSED THE WORD !!!");	
-		newGamePrompt();
-	}
-	
-	private static void guessOutcome(boolean letterGuessed, boolean repeated) {		
-		if (repeated) {
-			System.out.println("You already tried this one...");
-		} else if (letterGuessed) {
-			System.out.println("Good guess! Keep going!");
-		} else {
-			System.out.println("Sorry! This letter isn't in the word!");
-		}
-		
-		System.out.println();		
-	}
+	}		
 	
 	private static void newGamePrompt(){
 		System.out.print("Would you like to play again? (Y - yes; default - close game): ");
-	}
-	
-	public static void initializeNewGame() {
-		VisualControls.clearConsole(); 
-		VisualControls.printRules();
-		VisualControls.printCategorySelection();		
-	}
+	}	
 }
